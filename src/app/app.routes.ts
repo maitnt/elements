51056d1e049bf867d34266d2c1ee39c8f48388d6
trang.nguyen <maitrangnt2210@gmail.com>
2025-06-15 20:44:58 +0700

    chore: add comp timeline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TimelineComponent } from './timeline/timeline.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'timeline', component: TimelineComponent}
+];
