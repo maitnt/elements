4d250b2a96effb91aa3627635d099346a4b3aef9
trang.nguyen <maitrangnt2210@gmail.com>
2025-07-21 15:13:42 +0700

    style: type writer effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonSlideComponent } from './button-slide/button-slide.component';
+import { TypewriterEffectComponent } from './typewriter-effect/typewriter-effect.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonSlideComponent],
+  imports: [RouterOutlet, ButtonSlideComponent, TypewriterEffectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/typewriter-effect/typewriter-effect.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typewriter-effect',
+  standalone: true,
+  imports: [],
+  templateUrl: './typewriter-effect.component.html',
+  styleUrl: './typewriter-effect.component.scss'
+})
+export class TypewriterEffectComponent {
+
+}
